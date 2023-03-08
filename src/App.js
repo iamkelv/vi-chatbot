0f88cfdeb8a5153203acf32954e041f21d477761
Kelvin Moses <moseskelvin4u@gmail.com>
2023-03-08 21:33:19 +0100

    init

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Head } from './components/Head'
 function App() {
   return (
     <div className="w-screen h-screen bg-[rgb(40,44,52)] relative flex items-center">
-      <div className="w-[70%] tablet:w-[40%] desktop:w-[20%] p-2 mx-auto  relative flex flex-col  rounded-xl min-h-[80%] bg-white ">
+      <div className="w-[100%] h-[100vh] tablet:h-[80vh]  tablet:w-[40%] desktop:w-[20%] p-2 mx-auto  relative flex flex-col  rounded-xl min-h-[80%]  bg-white ">
         <Head />
         <Body />
         <Footer />
